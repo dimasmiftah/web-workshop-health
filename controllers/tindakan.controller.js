c4534509403792797d86e94e627a0c51eef63f02
Muhammad Jafar Shidik <62690663+mjafarshidik@users.noreply.github.com>
2022-06-20 21:20:49 +0700

    feat : add data on nurses, patients and actions

---
+++ controllers/tindakan.controller.js
@@ -0,0 +1,29 @@
+const Tindakan = require('../models/Tindakan');
+
+module.exports = {
+  index: async (req, res) => {
+    const daftar_tindakan = await Tindakan.findAll();
+    //menambahkan komentar
+    return res.render('tindakan/index', {
+      daftar_tindakan,
+    });
+  },
+
+  create: async (req, res) => {
+    return res.render('tindakan/create');
+  },
+
+  store: async (req, res) => {
+    await Tindakan.create({
+      No_Registrasi: req.body.No_Registrasi,
+      ID_Perawat: req.body.Id_Perawat,
+      Nama_Pasien: req.body.Nama_Pasien,
+      No_Ruang: req.body.No_Ruang,
+      Tindakan_Keperawatan: req.body.Tindakan_Keperawatan,
+      Jam: req.body.Jam,
+      Keterangan: req.body.Keterangan,
+    });
+
+    return res.redirect('/tindakan');
+  },
+};
